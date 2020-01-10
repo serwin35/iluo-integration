14a2452f80647f01289b9738a842e025808e7a59
serwin <biuro@blue-net.pl>
2020-01-10 23:09:45 +0100

    546

---
+++ config/iluo.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'host' => env('ILUO_HOST', NULL),
+
+    'port' => env('ILUO_PORT', NULL),
+
+    'account' => env('ILUO_ACCOUNT', NULL),
+
+    'token' => env('ILUO_TOKEN', NULL),
+
+];
